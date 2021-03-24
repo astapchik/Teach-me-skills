2517c2cf5a46e996c4e1250a33204e7f5d3dfcb6
LevSlinsen <14149705+ice-ivanov@users.noreply.github.com>
2021-03-24 20:15:20 +0200

    added settings with 3 types of data

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import os
-my_variable = os.environ['X']
-print(my_variable)
+# Settings for all things
+SETTINGS = (True, 2, '3')
+
+one = SETTINGS[0]
+two = SETTINGS[1]
+three = SETTINGS[2]
+
+print(two)
+print(type(two))
